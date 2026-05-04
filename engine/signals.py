dcffdf05ab0b0f9937e559690790e3609ca396e9
Vikasbsahu <62749314+Vikasbsahu@users.noreply.github.com>
2026-05-04 18:18:36 +0530

    Update signals.py

--- engine/signals.py
+++ engine/signals.py
@@ -5,7 +5,6 @@ def vix_signal(v):
     elif v < 35: return 85
     return 100
 
-
 def drawdown_signal(d):
     if d > -5: return 20
     elif d > -10: return 40
@@ -13,25 +12,15 @@ def drawdown_signal(d):
     elif d > -35: return 90
     return 100
 
-
 def dma_signal(d):
     if d > 0: return 20
     elif d > -3: return 45
     elif d > -7: return 75
     return 100
 
-
 def valuation_signal(v):
     if v > 15: return 20
     elif v > 5: return 45
     elif v > -5: return 70
     elif v > -10: return 90
-    return 100
-
-
-def rsi_signal(r):
-    if r > 70: return 20
-    elif r > 60: return 40
-    elif r > 40: return 60
-    elif r > 30: return 80
     return 100
